c1ce776df9d49fae889743866878bd613e185fd7
Suraj <suraj.ukumar.p@gmail.com>
2024-12-17 00:31:24 +0530

    feat: sinup setup.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,51 @@
-export const singup = (req, res) => {};
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.models.js";
+import bcrypt from "bcryptjs";
+
+export const singup = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res.status(400).json({ msg: "All fields are required" });
+    }
+    // hash password => for this we have installed  bcrypt js
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ msg: "Password must be at least 6 characters long" });
+    }
+
+    const user = await User.findOne({ email });
+    if (user) return res.status(400).json({ msg: "User already exists" });
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      fullName,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      // generate jwt token
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePicture: newUser.profilePicture,
+      });
+    } else {
+      res.status(400).json({ msg: "Invalid user data" });
+    }
+  } catch (err) {
+    console.log("Error in singup controller", err.message);
+    res.status(500).json({ msg: "Internal Server error" });
+  }
+};
 
 export const login = (req, res) => {
   res.send("login route");
