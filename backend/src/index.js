95b3896627b9a62a9dc57afbca3da063a5e53def
Suraj <suraj.ukumar.p@gmail.com>
2025-01-24 23:57:38 +0530

    core: implement message route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import prisma from "./lib/prisma.js";
@@ -26,6 +27,7 @@ app.use(cookieParser());
 
 // Routes
 app.use("/api/auth", authRoutes);
+app.use("api/message", messageRoutes);
 
 // Server startup
 app.listen(PORT, async () => {
