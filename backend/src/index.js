ddc08286265b236e449cf200bec76c2622179503
Suraj <suraj.ukumar.p@gmail.com>
2025-02-02 23:45:54 +0530

    feat: set up project to production

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,13 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { app, server } from "./lib/socket.js";
 import prisma from "./lib/prisma.js";
+import path from "path";
 
 dotenv.config();
 const PORT = process.env.PORT;
+
+// Define the directory name
+const __dirname = path.resolve();
 
 // ✅ Correct Middleware Usage
 app.use(
@@ -40,6 +44,15 @@ app.use("/api/messages", messageRoutes);
 // Configure request payload limits
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"))
+  );
+}
 
 // Server startup
 server.listen(PORT, async () => {
